bef5d9dfc076d85fbdc1f9dd2aadba29e8e4eefc
Rika Kamila <rikakamila1717@gmail.com>
2024-10-17 01:37:33 +0700

    Setting Middeleware n Validate Token

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,20 @@ const express = require("express");
 const route = express.Router();
 const todosRoute = require('./todos-route');
 const authRoute = require('./auth-route');
+const { validateToken} = require('../middleware/auth');
 
-
+route.use((req, res, next) => {
+    console.log('middleware untuk semua route');
+    next();
+  });
+  
 route.get("/", (req, res) => {
     res.json({
       message: "selamat datang di TodoList App",
     });
   });
 
-route.use('/todos', todosRoute );
+route.use('/todos', validateToken, todosRoute );
 route.use('/auth', authRoute);
 
 module.exports = route;
