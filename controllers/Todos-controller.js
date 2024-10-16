2ebea19502d53c9d54f8b6994563cb6212c26283
Rika Kamila <rikakamila1717@gmail.com>
2024-10-17 00:12:00 +0700

    Add JS getAllTodos, getTodosById, n addTodo

--- controllers/Todos-controller.js
+++ controllers/Todos-controller.js
@@ -2,11 +2,45 @@ const Todos = require('../models/Todos');
 module.exports = {
 
 // Get All Todos
-getAllTodos: async (req, res) => {},
+getAllTodos: async (req, res) => {
+    const data = await Todos.find({});
+
+    res.json({
+      message: "Get todos success",
+      data,
+    });
+},
 // Get Todos By Id
-getTodosById: (req, res) => {},
+getTodosById: (req, res) => {
+    const id = req.params.id;
+
+    Todos.findById(id)
+        .then(todo => {
+            if (!todo) {
+                return res.json({ message: 'Todo not found' });
+            }
+            res.json({
+                message: "Get todo by id success",
+                data: todo
+            });
+        })
+        .catch(err => {
+            console.error(err);
+            res.json({ message: 'Terjadi kesalahan saat mengambil Todo' });
+        });
+
+},
 // Add Todos
-addTodos: (req, res) => {},
+addTodos: (req, res) => {
+    const data = req.body;
+
+    const newTodos = new Todos(data);
+    newTodos.save();
+
+    res.json({
+      message: "Add Todo Success",
+    });
+  },
 // Edit Todos
 editTodosById: (req, res) => {},
 // Delete Todos
