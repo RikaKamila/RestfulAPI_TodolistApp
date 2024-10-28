91f0a2af1f2e6b38edae8c6193354a575bcb4b95
Rika Kamila <rikakamila1717@gmail.com>
2024-10-28 09:37:22 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,15 @@ const express = require("express");
 const route = express.Router();
 const todosRoute = require('./todos-route');
 const authRoute = require('./auth-route');
+// const {validateToken} = require('../middleware/auth')
 
+route.get("/", (req, res) => {
+    res.json({
+      message: "Welcome RestfullAPI TodoList App",
+    });
+  });
 
+// route.use('/todos', validateToken, todosRoute );
 route.use('/todos', todosRoute );
 route.use('/auth', authRoute);
 
